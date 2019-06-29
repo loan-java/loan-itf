89651fb85e503a631bd49b18815f17b4d717d40a
shuailj <178397218@qq.com>
2019-06-29 12:49:22 +0800

    配置优化

--- src/main/java/com/mod/loan/config/Constant.java
+++ src/main/java/com/mod/loan/config/Constant.java
@@ -50,32 +50,32 @@ public class Constant {
     public static String OSS_ENDPOINT_IN;
     public static String OSS_ACCESS_KEY_SECRET;
 
-    @Value("${rongze.request.app.id}")
+    @Value("${rongze.request.app.id:}")
     public void setRongZeRequestAppId(String rongZeRequestAppId) {
         Constant.rongZeRequestAppId = rongZeRequestAppId;
     }
 
-    @Value("${rongze.callback.url}")
+    @Value("${rongze.callback.url:}")
     public void setRongZeCallbackUrl(String rongZeCallbackUrl) {
         Constant.rongZeCallbackUrl = rongZeCallbackUrl;
     }
 
-    @Value("${rongze.query.url}")
+    @Value("${rongze.query.url:}")
     public void setRongZeQueryUrl(String rongZeQueryUrl) {
         Constant.rongZeQueryUrl = rongZeQueryUrl;
     }
 
-    @Value("${org.rsa.private.key}")
+    @Value("${org.rsa.private.key:}")
     public void setOrgPrivateKey(String orgPrivateKey) {
         Constant.orgPrivateKey = orgPrivateKey;
     }
 
-    @Value("${rongze.rsa.public.key}")
+    @Value("${rongze.rsa.public.key:}")
     public void setRongZePublicKey(String rongZePublicKey) {
         Constant.rongZePublicKey = rongZePublicKey;
     }
 
-    @Value("${sms.type}")
+    @Value("${sms.type:}")
     public void setSmsType(String smsType) {
         if(StringUtils.startsWith(smsType, "%")){
             try {
@@ -87,32 +87,32 @@ public class Constant {
         Constant.smsType = smsType;
     }
 
-    @Value("${kuaiqian.jks.path}")
+    @Value("${kuaiqian.jks.path:}")
     public void setKuaiQianJksPath(String kuaiQianJksPath) {
         Constant.kuaiQianJksPath = kuaiQianJksPath;
     }
 
-    @Value("${kuaiqian.key.password}")
+    @Value("${kuaiqian.key.password:}")
     public void setKuaiQianKeyPassword(String kuaiQianKeyPassword) {
         Constant.kuaiQianKeyPassword = kuaiQianKeyPassword;
     }
 
-    @Value("${kuaiqian.version}")
+    @Value("${kuaiqian.version:}")
     public void setKuaiQianVersion(String kuaiQianVersion) {
         Constant.kuaiQianVersion = kuaiQianVersion;
     }
 
-    @Value("${kuaiqian.repay.url}")
+    @Value("${kuaiqian.repay.url:}")
     public void setKuaiQianRepayUrl(String kuaiQianRepayUrl) {
         Constant.kuaiQianRepayUrl = kuaiQianRepayUrl;
     }
 
-    @Value("${kuaiqian.terminal.id}")
+    @Value("${kuaiqian.terminal.id:}")
     public void setKuaiQianTerminalId(String kuaiQianTerminalId) {
         Constant.kuaiQianTerminalId = kuaiQianTerminalId;
     }
 
-    @Value("${kuaiqian.member.id}")
+    @Value("${kuaiqian.member.id:}")
     public void setKuaiQianMemberId(String kuaiQianMemberId) {
         Constant.kuaiQianMemberId = kuaiQianMemberId;
     }
@@ -122,42 +122,42 @@ public class Constant {
         Constant.ENVIROMENT = environment;
     }
 
-    @Value("${baofoo.key.store.path}")
+    @Value("${baofoo.key.store.path:}")
     public void setBaoFooKeyStorePath(String baoFooKeyStorePath) {
         Constant.baoFooKeyStorePath = baoFooKeyStorePath;
     }
 
-    @Value("${baofoo.key.store.password}")
+    @Value("${baofoo.key.store.password:}")
     public void setBaoFooKeyStorePassword(String baoFooKeyStorePassword) {
         Constant.baoFooKeyStorePassword = baoFooKeyStorePassword;
     }
 
-    @Value("${baofoo.pub.key.path}")
+    @Value("${baofoo.pub.key.path:}")
     public void setBaoFooPubKeyPath(String baoFooPubKeyPath) {
         Constant.baoFooPubKeyPath = baoFooPubKeyPath;
     }
 
-    @Value("${baofoo.repay.url}")
+    @Value("${baofoo.repay.url:}")
     public void setBaoFooRepayUrl(String baoFooRepayUrl) {
         Constant.baoFooRepayUrl = baoFooRepayUrl;
     }
 
-    @Value("${baofoo.member.id}")
+    @Value("${baofoo.member.id:}")
     public void setBaoFooMemberId(String baoFooMemberId) {
         Constant.baoFooMemberId = baoFooMemberId;
     }
 
-    @Value("${baofoo.terminal.id}")
+    @Value("${baofoo.terminal.id:}")
     public void setBaoFooTerminalId(String baoFooTerminalId) {
         Constant.baoFooTerminalId = baoFooTerminalId;
     }
 
-    @Value("${baofoo.version}")
+    @Value("${baofoo.version:}")
     public void setBaoFooVersion(String baoFooVersion) {
         Constant.baoFooVersion = baoFooVersion;
     }
 
-    @Value("${juhe.call.back.url}")
+    @Value("${juhe.call.back.url:}")
     public void setJuheCallBackUrl(String juheCallBackUrl) {
         Constant.juheCallBackUrl = juheCallBackUrl;
     }
@@ -182,7 +182,7 @@ public class Constant {
         OSS_ACCESS_KEY_SECRET = oSS_ACCESS_KEY_SECRET;
     }
 
-    @Value("${oss.endpoint.in}")
+    @Value("${oss.endpoint.in:}")
     public void setOssEndpointIn(String ossEndpointIn) {
         Constant.OSS_ENDPOINT_IN = ossEndpointIn;
     }
